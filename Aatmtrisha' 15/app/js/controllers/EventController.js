3f8bba054e1ba1139ec2adbab939561e1e9d0e98
abhi4dev <abhi4pesit@gmail.com>
2015-01-03 13:14:38 +0530

    Voting For the Events
    Voting for the Events: UpCount and DownCount Finished

--- Aatmtrisha' 15/app/js/controllers/EventController.js
+++ Aatmtrisha' 15/app/js/controllers/EventController.js
@@ -2,7 +2,7 @@
 
 eventsApp.controller('EventController',
 	function EventController($scope) {
-		
+
 		$scope.event = {
 			name: "AatmTrisha'15",
 			date: '13-14 February 2015',
@@ -14,30 +14,42 @@ eventsApp.controller('EventController',
 				zipcode: '560085'
 			},
 			imageURL: 'img/AatmTrisha.png',
+			
 			sessions: [
 				{
 					name: 'AT got Talent ',
 					creatorName: ' Abhishek ',
 					duration: '3 hr',
 					level: 'Competition',
-					abstract: 'In this session...'
+					abstract: 'In this session...',
+					upVoteCount: 40
 				},
 				{
 					name: ' Pitch Please',
 					creatorName: 'Abhishek',
 					duration: '2 hr',
 					level: 'Competition',
-					abstract: 'In this session...'
+					abstract: 'In this session...',
+					upVoteCount: 27
 				},
 				{
 					name: 'Paisa Vassool',
 					creatorName: 'Abhishek',
 					duration: '2 hr',
 					level: 'Entertainment',
-					abstract: 'In this session...'
+					abstract: 'In this session...',
+					upVoteCount: 80
 				}
 			]
-		}
+		};
+
+
+		$scope.upVoteSession = function (session) {
+			session.upVoteCount++;
+		};
+		$scope.downVoteSession = function (session) {
+			session.upVoteCount--;
+		};
 	}
 
 );
