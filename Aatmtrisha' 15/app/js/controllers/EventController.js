8d813978256ff24435142df5afdad260fabf57e9
abhi4dev <abhi4pesit@gmail.com>
2015-01-03 08:56:04 +0530

    Aatmatrisha' 15 App
    Aatmatrisha' 15 Event App For Event Registration

---
+++ Aatmtrisha' 15/app/js/controllers/EventController.js
@@ -0,0 +1,43 @@
+'use strict';
+
+eventsApp.controller('EventController',
+	function EventController($scope) {
+		
+		$scope.event = {
+			name: 'Angular Learning',
+			date: '09/12/2014',
+			time: '10:30am',
+			location: {
+				address: 'Bangalore',
+				city: 'Bangalore',
+				state: 'Bangalore',
+				zipcode: '560085'
+			},
+			imageURL: 'img/angularjs-logo.jpeg',
+			sessions: [
+				{
+					name: 'Javascript Basics',
+					creatorName: 'Abishek ',
+					duration: '1 hr',
+					level: 'Intermediate and Advanced',
+					abstract: 'In this session...'
+				},
+				{
+					name: 'Python Basics',
+					creatorName: 'Abhishek',
+					duration: '30 min',
+					level: 'Introductory',
+					abstract: 'In this session...'
+				},
+				{
+					name: 'JQuery',
+					creatorName: 'Abhishek',
+					duration: '2 hr',
+					level: 'Intermediate',
+					abstract: 'In this session...'
+				}
+			]
+		}
+	}
+
+);
